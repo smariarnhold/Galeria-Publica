dfcc33b8c7f53e134af7d33d63775fc9d8a8ec3a
malu <maluarnholdsampaio@gmail.com>
2025-01-24 08:36:29 -0300

    trabalho finalizado

--- app/src/main/java/arnhold/maria/galeriapublica/GridAdapter.java
+++ app/src/main/java/arnhold/maria/galeriapublica/GridAdapter.java
@@ -31,15 +31,6 @@ public class GridAdapter extends PagingDataAdapter<ImageData, MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         ImageData imageData = getItem(position);
 
-        TextView tvName = holder.itemView.findViewById(R.id.tvName);
-        tvName.setText(imageData.fileName);
-
-        TextView tvDate = holder.itemView.findViewById(R.id.tvDate);
-        tvDate.setText("Data: " + new SimpleDateFormat("HH:mm dd/MM/yyyy").format(imageData.date));
-
-        TextView tvSize = holder.itemView.findViewById(R.id.tvSize);
-        tvSize.setText("Tamanho: " + String.valueOf(imageData.size));
-
         Bitmap thumb = imageData.thumb;
         ImageView imageView = holder.itemView.findViewById(R.id.imThumb);
         imageView.setImageBitmap(thumb);
--- app/src/main/java/arnhold/maria/galeriapublica/MainActivity.java
+++ app/src/main/java/arnhold/maria/galeriapublica/MainActivity.java
@@ -46,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+
         //obtendo a referencia do MainViewMode
         final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
 
@@ -88,9 +92,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        List<String> permissions = new ArrayList<>();
-        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        checkForPermissions(permissions);
+
     }
 
     private void checkForPermissions(List<String> permissions) {
